7cdd50acba5e6a3e55f99ec040272b1baa7cc77e
吕小城 <lvxc@hsmap.com>
2021-04-25 18:39:01 +0800

    feat(模块添加): 模块添加依赖
    模块添加依赖

---
+++ spring-boot-web/src/main/java/com/module/web/SpringBootModuleApplication.java
@@ -0,0 +1,13 @@
+package com.module.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootModuleApplication.class, args);
+	}
+
+}
