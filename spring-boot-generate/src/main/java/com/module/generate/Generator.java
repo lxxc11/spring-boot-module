a7bddd79ed54583c8d15b34242929d710eda5d86
吕小城 <lvxc@hsmap.com>
2021-04-27 20:03:11 +0800

    feat(代码生成模块): 引入代码生成工具
    引入代码生成工具（精简版）

---
+++ spring-boot-generate/src/main/java/com/module/generate/Generator.java
@@ -0,0 +1,144 @@
+package com.module.generate;
+
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.po.TableInfo;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Description
+ * @Author lvxc
+ * @Date 2021/4/27 16:32
+ **/
+public class Generator {
+
+    public static void main(String[] args) {
+        // 代码生成器
+        AutoGenerator mpg = new AutoGenerator();
+        // 全局配置
+        mpg.setGlobalConfig(getGlobalConfig());
+        // 数据源配置
+        mpg.setDataSource(getDataSourceConfig());
+        // 包配置
+        mpg.setPackageInfo(getPackageConfig());
+        // 策略配置
+        mpg.setStrategy(getStrategyConfig());
+        // 执行代码生成器
+        mpg.execute();
+
+
+        // 自定义配置
+//        InjectionConfig cfg = new InjectionConfig() {
+//            @Override
+//            public void initMap() {
+//                // to do nothing
+//            }
+//        };
+
+        // 如果模板引擎是 freemarker
+        // String templatePath = "/templates/mapper.xml.ftl";
+        //如果模板引擎是 velocity
+//        String templatePath = "/templates/mapper.xml.vm";
+
+        // 自定义输出配置
+//        List<FileOutConfig> focList = new ArrayList<>();
+//        // 自定义配置会被优先输出
+//        focList.add(new FileOutConfig(templatePath) {
+//            @Override
+//            public String outputFile(TableInfo tableInfo) {
+//                // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
+//                return projectPath + "/src/main/resources/mapper/" + pc.getModuleName()
+//                        + "/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
+//            }
+//        });
+        /*
+        cfg.setFileCreate(new IFileCreate() {
+            @Override
+            public boolean isCreate(ConfigBuilder configBuilder, FileType fileType, String filePath) {
+                // 判断自定义文件夹是否需要创建
+                checkDir("调用默认方法创建的目录，自定义目录用");
+                if (fileType == FileType.MAPPER) {
+                    // 已经生成 mapper 文件判断存在，不想重新生成返回 false
+                    return !new File(filePath).exists();
+                }
+                // 允许生成模板文件
+                return true;
+            }
+        });
+        */
+//        cfg.setFileOutConfigList(focList);
+//        mpg.setCfg(cfg);
+
+        // 配置模板
+//        TemplateConfig templateConfig = new TemplateConfig();
+
+        // 配置自定义输出模板
+        //指定自定义模板路径，注意不要带上.ftl/.vm, 会根据使用的模板引擎自动识别
+//         templateConfig.setEntity("templates/entity2.java");
+        // templateConfig.setService();
+        // templateConfig.setController();
+
+//        templateConfig.setXml(null);
+//        mpg.setTemplate(templateConfig);
+
+
+//        mpg.setTemplateEngine(new FreemarkerTemplateEngine());
+    }
+
+    private static GlobalConfig getGlobalConfig(){
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig();
+        String projectPath = System.getProperty("user.dir");
+        gc.setOutputDir(projectPath + "/spring-boot-generate/src/main/java");
+        gc.setAuthor("lvxc");
+        //是否打开输出目录
+        gc.setOpen(false);
+        //是否覆盖原有文件,建议关闭
+        gc.setFileOverride(false);
+        // gc.setSwagger2(true); 实体属性 Swagger2 注解
+        return gc;
+    }
+
+    private static DataSourceConfig getDataSourceConfig(){
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setUrl("jdbc:mysql://192.168.110.220:3306/standard-investment?useUnicode=true&characterEncoding=UTF-8&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
+        dsc.setDriverName("com.mysql.cj.jdbc.Driver");
+        dsc.setUsername("root");
+        dsc.setPassword("fs2017");
+        return dsc;
+    }
+
+    private static PackageConfig getPackageConfig(){
+        PackageConfig pc = new PackageConfig();
+        //如需设置对模块进行分类可进行设置（多应用、多模块场景）
+        // pc.setModuleName();
+        pc.setEntity("model");
+        pc.setParent("com.module.generate");
+        return pc ;
+    }
+
+    private static StrategyConfig getStrategyConfig(){
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setNaming(NamingStrategy.underline_to_camel);
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+//        strategy.setSuperEntityClass("你自己的父类实体,没有就不用设置!");
+//        strategy.setEntityLombokModel(true);
+//        strategy.setRestControllerStyle(true);
+        // 公共父类
+//        strategy.setSuperControllerClass("你自己的父类控制器,没有就不用设置!");
+        // 写于父类中的公共字段
+        strategy.setSuperEntityColumns("id");
+        //表名
+        strategy.setInclude("zs_sys_user");
+        strategy.setControllerMappingHyphenStyle(true);
+        strategy.setTablePrefix("zs_","map_","im_");
+        return strategy ;
+    }
+}
