29b94a26837f74edb82941041315c9a8b70b9b7d
吕小城 <lvxc@hsmap.com>
2021-04-28 10:50:38 +0800

    feat(代码生成模块): 完善模板生成功能
    1.自定义xml生成
    2.支持lombok和swagger2
    3.模板可自定义

--- spring-boot-generate/src/main/java/com/module/generate/Generator.java
+++ spring-boot-generate/src/main/java/com/module/generate/Generator.java
@@ -4,10 +4,12 @@ import com.baomidou.mybatisplus.core.toolkit.StringPool;
 import com.baomidou.mybatisplus.generator.AutoGenerator;
 import com.baomidou.mybatisplus.generator.InjectionConfig;
 import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.builder.ConfigBuilder;
 import com.baomidou.mybatisplus.generator.config.po.TableInfo;
+import com.baomidou.mybatisplus.generator.config.rules.FileType;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
-import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,54 +33,10 @@ public class Generator {
         mpg.setStrategy(getStrategyConfig());
         // 模板生成配置
         mpg.setTemplate(getTemplateConfig());
+        // 自定义配置
+        mpg.setCfg(getInjectionConfig());
         // 执行代码生成器
         mpg.execute();
-
-
-        // 自定义配置
-//        InjectionConfig cfg = new InjectionConfig() {
-//            @Override
-//            public void initMap() {
-//                // to do nothing
-//            }
-//        };
-
-        // 如果模板引擎是 freemarker
-        // String templatePath = "/templates/mapper.xml.ftl";
-        //如果模板引擎是 velocity
-//        String templatePath = "/templates/mapper.xml.vm";
-
-        // 自定义输出配置
-//        List<FileOutConfig> focList = new ArrayList<>();
-//        // 自定义配置会被优先输出
-//        focList.add(new FileOutConfig(templatePath) {
-//            @Override
-//            public String outputFile(TableInfo tableInfo) {
-//                // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
-//                return projectPath + "/src/main/resources/mapper/" + pc.getModuleName()
-//                        + "/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
-//            }
-//        });
-        /*
-        cfg.setFileCreate(new IFileCreate() {
-            @Override
-            public boolean isCreate(ConfigBuilder configBuilder, FileType fileType, String filePath) {
-                // 判断自定义文件夹是否需要创建
-                checkDir("调用默认方法创建的目录，自定义目录用");
-                if (fileType == FileType.MAPPER) {
-                    // 已经生成 mapper 文件判断存在，不想重新生成返回 false
-                    return !new File(filePath).exists();
-                }
-                // 允许生成模板文件
-                return true;
-            }
-        });
-        */
-//        cfg.setFileOutConfigList(focList);
-//        mpg.setCfg(cfg);
-
-
-
 
 //        mpg.setTemplateEngine(new FreemarkerTemplateEngine());
     }
@@ -93,7 +51,7 @@ public class Generator {
         gc.setOpen(false);
         //是否覆盖原有文件,建议关闭
         gc.setFileOverride(false);
-        // gc.setSwagger2(true); 实体属性 Swagger2 注解
+        gc.setSwagger2(true);
         return gc;
     }
 
@@ -118,29 +76,68 @@ public class Generator {
     private static StrategyConfig getStrategyConfig(){
         // 策略配置
         StrategyConfig strategy = new StrategyConfig();
+        // 表名
+        strategy.setInclude("zs_sys_user");
+        strategy.setTablePrefix("zs_","map_","im_");
         strategy.setNaming(NamingStrategy.underline_to_camel);
         strategy.setColumnNaming(NamingStrategy.underline_to_camel);
-//        strategy.setSuperEntityClass("你自己的父类实体,没有就不用设置!");
-//        strategy.setEntityLombokModel(true);
-//        strategy.setRestControllerStyle(true);
-        // 公共父类
-//        strategy.setSuperControllerClass("你自己的父类控制器,没有就不用设置!");
-        // 写于父类中的公共字段
-        strategy.setSuperEntityColumns("id");
-        //表名
-        strategy.setInclude("zs_sys_user");
-        strategy.setControllerMappingHyphenStyle(true);
-        strategy.setTablePrefix("zs_","map_","im_");
+        strategy.setEntityLombokModel(true);
+        // 生成字段常量
+        strategy.setEntityColumnConstant(true);
         return strategy ;
     }
 
     private static TemplateConfig getTemplateConfig(){
         // 配置模板
         TemplateConfig templateConfig = new TemplateConfig();
-//         templateConfig.setEntity("templates/entity2.java");
+        //自定义模板
+//        templateConfig.setEntity("templates/entity2.java");
         templateConfig.setService(null);
         templateConfig.setController(null);
         templateConfig.setServiceImpl(null);
+        //自定义XML输出路径后这里可以设置为null
+        templateConfig.setXml(null);
         return templateConfig ;
+    }
+
+    private static InjectionConfig getInjectionConfig(){
+        InjectionConfig cfg = new InjectionConfig() {
+            @Override
+            public void initMap() {
+                // to do nothing
+            }
+        };
+
+        // 如果模板引擎是 freemarker
+        // String templatePath = "/templates/mapper.xml.ftl";
+        // velocity模板引擎
+        String templatePath = "/templates/mapper.xml.vm";
+
+        // 自定义输出配置
+        List<FileOutConfig> focList = new ArrayList<>();
+        // 自定义配置会被优先输出
+        focList.add(new FileOutConfig(templatePath) {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
+                return System.getProperty("user.dir") + "/spring-boot-generate/src/main/resources/mapper/"
+                         + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
+            }
+        });
+        cfg.setFileCreate(new IFileCreate() {
+            @Override
+            public boolean isCreate(ConfigBuilder configBuilder, FileType fileType, String filePath) {
+                // 判断自定义文件夹是否需要创建
+                checkDir(filePath);
+                if (fileType == FileType.MAPPER) {
+                    // 已经生成 mapper 文件判断存在，不想重新生成返回 false
+                    return !new File(filePath).exists();
+                }
+                // 允许生成模板文件
+                return true;
+            }
+        });
+        cfg.setFileOutConfigList(focList);
+        return cfg ;
     }
 }
