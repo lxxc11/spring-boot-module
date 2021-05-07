498a70aee345de0b7ad8a050eafd5d35fd5efddc
吕小城 <lvxc@hsmap.com>
2021-05-07 19:16:56 +0800

    feat(gradlew): gradlew build 可执行
    gradlew build 可执行

--- spring-boot-web/src/main/java/com/module/web/vo/UserVo.java
+++ spring-boot-web/src/main/java/com/module/web/vo/UserVo.java
@@ -1,5 +1,7 @@
 package com.module.web.vo;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import org.springframework.validation.annotation.Validated;
 
@@ -11,11 +13,15 @@ import java.io.Serializable;
  * @Date 2021/5/6 10:17
  **/
 @Data
+@ApiModel
 public class UserVo  implements Serializable {
 
+     @ApiModelProperty(value = "姓名")
      private String name ;
 
+     @ApiModelProperty(value = "密码")
      private String password ;
 
+     @ApiModelProperty(value = "邮件")
      private String email ;
 }
